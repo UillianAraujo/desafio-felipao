b62cc00b3a1e18eeba8e1403fec5816153528a86
UillianAraujo <152391130+UillianAraujo@users.noreply.github.com>
2025-02-12 23:22:04 -0300

    alteraçao funçao

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
-let nomeHeroi = ""
+let nomeHeroi = "POMBO VOADOR"
+let xp = "7580"
 
-let xp = ""
-
-if(xp <= 1000){
-    nivel = "FERRO"
-} else if(xp >= 1001 && xp <= 2000){
-    nivel = "BRONZE"
-} else if(xp >= 2001 && xp <= 5000){
-    nivel = "PRATA"
-} else if(xp >= 5001 && xp <= 7000){
-    nivel = "OURO"
-} else if(xp >= 7001 && xp <= 8000){
-    nivel = "PLATINA"
-} else if(xp >= 8001 && xp <= 9000){
-    nivel = "ASCENDENTE"
-} else if(xp >= 9001 && xp <= 10000){
-    nivel = "RADIANTE"
-} else{
-    nivel = "IMORTAL"
-
+function determinarNivel(xp) {
+    let nivel;
+    switch (true) {
+        case (xp <= 1000):
+            nivel = "FERRO";
+            break
+        case (xp >= 1001 && xp <= 2000):
+            nivel = "BRONZE"
+            break
+        case (xp >= 2001 && xp <= 5000):
+            nivel = "PRATA"
+            break
+        case (xp >= 5001 && xp <= 7000):
+            nivel = "OURO"
+            break
+        case (xp >= 7001 && xp <= 8000):
+            nivel = "PLATINA"
+            break
+        case (xp >= 8001 && xp <= 9000):
+            nivel = "ASCENDENTE"
+            break;
+        case (xp >= 9001 && xp <= 10000):
+            nivel = "RADIANTE"
+            break
+        default:
+            nivel = "IMORTAL"
+    }
+    return nivel;
 }
 
+let nivel = determinarNivel(xp);
 console.log("O Herói " + nomeHeroi + " está no nível " + nivel + ". ")
