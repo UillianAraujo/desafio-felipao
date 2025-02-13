e14b21aaf986f6b10a1bba73cad6be90faa2851b
UillianAraujo <uillsilva52@gmail.com>
2025-02-12 22:34:21 -0300

    desafio do felipao

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-let nome = "Uillian Araujo"
-console.log(nome)
+let nomeHeroi = ""
+
+let xp = ""
+
+if(xp <= 1000){
+    nivel = "FERRO"
+} else if(xp >= 1001 && xp <= 2000){
+    nivel = "BRONZE"
+} else if(xp >= 2001 && xp <= 5000){
+    nivel = "PRATA"
+} else if(xp >= 5001 && xp <= 7000){
+    nivel = "OURO"
+} else if(xp >= 7001 && xp <= 8000){
+    nivel = "PLATINA"
+} else if(xp >= 8001 && xp <= 9000){
+    nivel = "ASCENDENTE"
+} else if(xp >= 9001 && xp <= 10000){
+    nivel = "RADIANTE"
+} else{
+    nivel = "IMORTAL"
+
+}
+
+console.log("O Herói " + nomeHeroi + " está no nível " + nivel + ". ")
